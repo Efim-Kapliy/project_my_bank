d0473d990ccfd48823a64763a21d4b6bfc696dbc
Efim <efim0424@mail.ru>
2024-12-09 14:10:50 +0300

    feat: add RQuery library

--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -3,6 +3,7 @@ import renderService from '@/core/services/render.service'
 
 import template from './home.template.html'
 import styles from './home.module.scss'
+import $R from '@/core/rquery/rquery.lib'
 
 export class Home extends BaseScreen {
 	constructor() {
@@ -10,9 +11,9 @@ export class Home extends BaseScreen {
 	}
 
 	render() {
-		console.log(styles)
 		const element = renderService.htmlToElement(template, [], styles)
 
+		$R(element).find('h1').css('color', 'black')
 		return element.outerHTML
 	}
 }
