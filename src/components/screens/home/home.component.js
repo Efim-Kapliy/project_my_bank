c2e4946d16c0e1affad6140fc038c8a5d4d056d5
Efim <efim0424@mail.ru>
2024-12-13 21:18:12 +0300

    feat: add Heading, UserItem, Loader components

--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -4,6 +4,7 @@ import renderService from '@/core/services/render.service'
 
 import { Button } from '@/components/ui/button/button.component'
 import { Field } from '@/components/ui/field/field.component'
+import { UserItem } from '@/components/ui/user-item/user-item.component'
 
 import styles from './home.module.scss'
 import template from './home.template.html'
@@ -22,7 +23,16 @@ export class Home extends BaseScreen {
 					placeholder: 'Number...',
 					type: 'text',
 					variant: 'green'
-				})
+				}),
+				new UserItem(
+					{
+						avatarPath:
+							'https://i.pinimg.com/736x/b7/5b/29/b75b29441bbd967deda4365441497221.jpg',
+						name: 'Max'
+					},
+					false,
+					() => alert('Hey')
+				)
 			],
 			styles
 		)
