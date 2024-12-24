be47ec9c686587ea6386aea02b1dba2792e22448
Efim <efim0424@mail.ru>
2024-12-24 18:47:44 +0300

    feat: finalize login

--- src/core/rquery/rquery.lib.js
+++ src/core/rquery/rquery.lib.js
@@ -170,6 +170,25 @@ class RQuery {
 	/* END FORM */
 
 	/* START STYLES */
+
+	/**
+	 * Shows the selected element by removing the 'display' style property.
+	 * @returns {RQuery} The current RQuery instance for chaining.
+	 */
+	show() {
+		this.element.style.removeProperty('display')
+		return this
+	}
+
+	/**
+	 * Hides the selected element by setting it's display style to 'none'.
+	 * @returns {RQuery} The current RQuery instance for chaining.
+	 */
+	hide() {
+		this.element.style.display = 'none'
+		return this
+	}
+
 	/**
 	 * Set the CSS style of the selected element.
 	 * @param {string} property - The CSS property to set.
